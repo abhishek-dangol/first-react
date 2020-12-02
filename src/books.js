35a2e1c5226bad9a151f77814ab3394c93f22386
Abhishek Dangol <abhi.dangol@gmail.com>
2020-12-01 23:19:20 -0600

    first commit

---
+++ src/books.js
@@ -0,0 +1,25 @@
+export const data = [
+  {
+    id: 1,
+    author: "Barack Obama",
+    title: "A Promised Land",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg",
+  },
+
+  {
+    id: 2,
+    author: "Ernest Cline",
+    title: "Ready Player Two: A Novel",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/811W9hHXiwL._AC_UL200_SR200,200_.jpg",
+  },
+
+  {
+    id: 3,
+    author: "Dr. Seuss",
+    title: "How the Grinch Stole Christmas",
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91Q9eneR7BL._AC_UL200_SR200,200_.jpg",
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+//CSS
+import "./index.css";
+
+import { data } from "./books";
+import Book from "./Book";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      {data.map((book, index) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+
+ReactDom.render(<BookList />, document.getElementById("root"));
